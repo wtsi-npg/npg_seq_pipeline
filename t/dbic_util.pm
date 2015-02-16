d7c69483caf5c9f5e9b364c06e9aa1684d072f29
Marina Gourtovaia <mg8@sanger.ac.uk>
2015-02-16 14:52:09 +0000

    use in-memory sqlite test database

--- t/dbic_util.pm
+++ t/dbic_util.pm
@@ -1,8 +1,6 @@
 package t::dbic_util;
 
 use Moose;
-use File::Temp qw{ tempdir };
-use Test::More;
 my $DEFAULT_FIXTURE_PATH = './t/data/dbic_fixtures';
 
 with 'npg_testing::db';
@@ -13,25 +11,12 @@ has 'fixture_path' => (
     default => $DEFAULT_FIXTURE_PATH,
 );
 
-has '_db_temp_dir' => (
-    is      => 'ro',
-    isa     => 'Str',
-    default => sub { tempdir(CLEANUP => 1) },
-);
-
-sub _db_file_name {
-    my ( $self, $name ) = @_;
-    my $db_file_name = join q[/], $self->_db_temp_dir, $name;
-    note $db_file_name;
-    return $db_file_name;
-}
-
 sub test_schema {
     my ($self) = @_;
     return $self->create_test_db(
         'npg_tracking::Schema',
         $self->fixture_path(),
-        $self->_db_file_name('npg_tracking')
+        ':memory:'
     );
 }
 
@@ -40,7 +25,7 @@ sub test_schema_mlwh {
     return $self->create_test_db(
         'WTSI::DNAP::Warehouse::Schema',
         $fixture_path,
-        $self->_db_file_name('mlwh')
+        ':memory:'
     );
 }
 
