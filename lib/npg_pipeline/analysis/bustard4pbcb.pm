7a20fe7588bd2d44aad9d4e083b0920ff19d6094
David K Jackson <david.jackson@sanger.ac.uk>
2015-12-16 23:01:25 +0000

    npg_pipeline_job_env_to_threads replaces perl one-liners

--- lib/npg_pipeline/analysis/bustard4pbcb.pm
+++ lib/npg_pipeline/analysis/bustard4pbcb.pm
@@ -170,7 +170,7 @@ sub _make_command {
     ##use critic
   });
   if ($deps) { push @command, $deps; }
-  push @command, q['make -j `]. q[perl -e '"'"'print scalar(()=$].q[ENV{LSB_BIND_CPU_LIST}=~/\d+/smg) || $].q[ENV{LSB_MCPU_HOSTS}=~/(\d+)\s*\Z/sm;'"'"'] .qq[` $target'];
+  push @command, q['make -j `npg_pipeline_job_env_to_threads` ] . qq[$target'];
   return join q[ ], @command;
 }
 
