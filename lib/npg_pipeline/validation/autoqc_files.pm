22d14b10f065c1927b560f61241c6f0908c327d5
Marina Gourtovaia <mg8@sanger.ac.uk>
2018-01-05 12:53:34 +0000

    run_is_deletable script and related files copied from teh svn data_handling project

---
+++ lib/npg_pipeline/validation/autoqc_files.pm
@@ -0,0 +1,428 @@
+package npg_validation::runfolder::deletable::autoqc;
+
+use Moose;
+use Readonly;
+use Carp;
+use Try::Tiny;
+use List::MoreUtils qw/none/;
+
+use npg_qc::Schema;
+use npg_qc::autoqc::role::result;
+use npg_common::irods::Loader;
+
+with qw{npg_common::irods::iRODSCapable
+        npg_common::irods::Repository
+        npg_tracking::glossary::run};
+
+our $VERSION = do { my ($r) = q$Revision: 18294 $ =~ /(\d+)/mxs; $r; };
+
+Readonly::Scalar my $NO_TAG         => -1;
+Readonly::Scalar my $DEFAULT_VALUE  => 'default_value';
+
+Readonly::Array  my @COMMON_CHECKS        => qw/ qX_yield
+                                                 adapter
+                                                 gc_fraction
+                                                 insert_size
+                                                 ref_match
+                                                 sequence_error
+                                                 fastqcheck
+                                               /;
+
+Readonly::Array  my @LANE_LEVELCHECKS4POOL => qw/ tag_metrics
+                                                  upstream_tags
+                                                /;
+
+Readonly::Array  my @WITH_SUBSET_CHECKS =>    qw/ bam_flagstats
+                                                  samtools_stats
+                                                  sequence_summary
+                                                /;
+
+has 'exclude_bam'   => (isa           => q{Bool},
+                        is            => q{rw},
+                        documentation => q{flag to exclude bam file loading},
+                       );
+
+has 'skip_checks'   => ( isa           => 'ArrayRef',
+                         is            => 'ro',
+                         required      => 0,
+                         default       => sub { [] },
+                       );
+
+has 'irods_files'   => ( isa           => 'ArrayRef',
+                         is            => 'ro',
+                         required      => 0,
+                         lazy_build    => 1,
+                       );
+sub _build_irods_files {
+  my $self = shift;
+  my $bam_re;
+  if ($self->exclude_bam) {
+    $bam_re = q(cram$);
+  } else {
+    $bam_re = q(bam$);
+  }
+  my @files = ();
+  try {
+    @files = grep { /$bam_re/msx }
+      keys %{npg_common::irods::Loader->new
+        (irods => $self->irods)->get_collection_file_list
+          ($self->run_collection)};
+  } catch {
+    carp qq[Error getting a list of bam files form IRODs: $_];
+  };
+  return \@files;
+}
+
+has 'is_paired_read' => ( isa           => 'Bool',
+                          is            => 'ro',
+                          required      => 0,
+                          lazy_build    => 1,
+                        );
+
+sub _build_is_paired_read {
+  my $self = shift;
+  if( !@{$self->irods_files} ){
+    croak qq[No irods file list for run $self->id_run];
+  }
+  my $paired = 0;
+  foreach my $file (@{$self->irods_files}) {
+    my $irods_file = File::Spec->catfile($self->run_collection, $file);
+    my $remote_meta_data = npg_common::irods::Loader->new
+      (irods => $self->irods)->_check_meta_data($irods_file);
+    if (!exists($remote_meta_data->{is_paired_read})) {
+      croak qq[No is_paired_read meta data for irods file $irods_file];
+    }
+    my $values_in_irods = $remote_meta_data->{is_paired_read};
+    my @current_meta_values = keys %{$values_in_irods};
+    if (!scalar @current_meta_values) {
+      croak qq[No is_paired_read values for irods file $irods_file];
+    } elsif (scalar @current_meta_values > 1) {
+      croak qq[Multiple is_paired_read values for irods file $irods_file];
+    }
+    $paired = $current_meta_values[0];
+    last;
+  }
+  return $paired;
+}
+
+sub fully_archived {
+  my $self = shift;
+
+  try {
+    $self->_qc_schema;
+  } catch {
+    carp qq[Cannot connect to qc database: $_];
+    return 0;
+  };
+
+  my $count = scalar @{$self->_queries};
+  if ($count == 0) {
+    carp 'No queries to run for autoqc';
+    return 0;
+  }
+
+  my $skip_checks = $self->_parse_excluded_checks();
+
+  foreach my $query (@{$self->_queries}) {
+    my $skip = $self->_query_to_be_skipped($query, $skip_checks);
+    if ($self->verbose) {
+      warn sprintf '%s "%s"%s',
+        $skip ? 'Skipping' : 'Executing query for ',
+        $self->_query2string($query),
+        qq[\n];
+    }
+    $count = $count - ( $skip || $self->_result_exists($query) );
+  }
+  return !$count; #if all results exist, $count should be zero at the end
+}
+
+sub _parse_excluded_checks {
+  my $self = shift;
+
+  my $skip_checks = {};
+
+  foreach my $check ( @{$self->skip_checks} ) {
+    my @parsed = split /\+/smx, $check;
+    my $name = shift @parsed;
+    $skip_checks->{$name} = \@parsed;
+  }
+
+  return $skip_checks;
+}
+
+sub _query_to_be_skipped {
+  my ($self, $query, $skip_checks) = @_;
+
+  my $check_name = $query->{'check'};
+  my $skip = exists $skip_checks->{$check_name} ? 1 : 0;
+  my $skip_subset = $skip_checks->{$check_name};
+  if ( $skip_subset && @{$skip_subset} &&
+    ( !$query->{'subset'} || none { $query->{'subset'} eq $_ } @{$skip_subset}) ) {
+    $skip = 0;
+  }
+
+  return $skip;
+}
+
+has '_qc_schema'   =>  ( isa           => 'npg_qc::Schema',
+                         is            => 'ro',
+                         required      => 0,
+                         lazy_build    => 1,
+                       );
+sub _build__qc_schema {
+  my $self = shift;
+  return npg_qc::Schema->connect();
+}
+
+has '_catalogue'          => ( isa           => 'HashRef',
+                               is            => 'ro',
+                               required      => 0,
+                               lazy_build     => 1,
+                             );
+sub _build__catalogue {
+  my $self = shift;
+  return $self->file_catalogue($self->irods_files, $DEFAULT_VALUE, $DEFAULT_VALUE);
+}
+
+has '_queries'            => ( isa           => 'ArrayRef',
+                               is            => 'ro',
+                               required      => 0,
+                               lazy_build     => 1,
+                             );
+sub _build__queries {
+  my $self = shift;
+
+  my @queries = ();
+
+  foreach my $position ( keys %{$self->_catalogue} ) {
+
+    my $lane_is_plexed = !exists $self->_catalogue->{$position}->{$DEFAULT_VALUE};
+    my $query = {'position' => $position};
+    $query->{'tag_index'} =  _value4query($DEFAULT_VALUE);
+
+    if ( $lane_is_plexed ) {
+      ## no critic (BuiltinFunctions::ProhibitComplexMappings)
+      ## no critic (ValuesAndExpressions::ProhibitCommaSeparatedStatements)
+      push @queries,
+        map { my %q; %q = %{$query}, $q{'check'} = $_; \%q; }
+        (@LANE_LEVELCHECKS4POOL, @COMMON_CHECKS);
+    }
+
+    $query->{'subset'}    =  _value4query($DEFAULT_VALUE);
+    foreach my $tag_index (keys %{$self->_catalogue->{$position}}) {
+      foreach my $split ( keys %{$self->_catalogue->{$position}->{$tag_index}} ) {
+        my @checks = @WITH_SUBSET_CHECKS;
+        if ($split eq $DEFAULT_VALUE) {
+          push @checks, @COMMON_CHECKS;
+	} elsif ($split eq 'phix' ||
+            ($split =~ /human/smx && !$self->_catalogue->{$position}->{$tag_index}->{'phix'})) {
+          push @checks, 'alignment_filter_metrics';
+        }
+        ## no critic (BuiltinFunctions::ProhibitComplexMappings)
+        ## no critic (ValuesAndExpressions::ProhibitCommaSeparatedStatements)
+        push @queries,
+          map { my %q; %q = %{$query},
+                $q{'check'}     = $_;
+                _values2query(\%q, $tag_index, $split);
+                \%q;
+              }
+          @checks;
+      }
+    }
+  }
+  return \@queries;
+}
+
+sub _value4query {
+  my $value = shift;
+  return $value eq $DEFAULT_VALUE ? undef : $value;
+}
+
+sub _values2query {
+  my ($q, $tag_index, $subset) = @_;
+
+  my $check_name = $q->{'check'};
+  $q->{'tag_index'} = _value4query($tag_index);
+
+  if ( none { $_ eq $check_name } @WITH_SUBSET_CHECKS ) {
+    delete $q->{'subset'};
+  } else {
+    $q->{'subset'} = _value4query($subset);
+  }
+
+  return;
+}
+
+sub _result_exists {
+  my ($self, $query) = @_;
+
+  my $desc = $self->_query2string($query);
+  my $check_name = delete $query->{'check'};
+  my ($name, $class_name) = npg_qc::autoqc::role::result->class_names($check_name);
+
+  $query->{'id_run'} = $self->id_run;
+
+  if ($check_name eq 'fastqcheck') {
+    $query->{'tag_index'} = $query->{'tag_index'} // $NO_TAG;
+    my $count = $self->_qc_schema->resultset($class_name)->search($query)->count;
+    my $pool = !exists $self->_catalogue->{$query->{'position'}}->{$DEFAULT_VALUE};
+    my $expected = 1;
+    ## no critic (ControlStructures::ProhibitPostfixControls)
+    $expected++ if ($pool && ($query->{'tag_index'} == $NO_TAG));
+    $expected++ if $self->is_paired_read;
+    ## use critic
+    if ($count != $expected) {
+      carp qq[Expected $expected results got $count for "$desc"];
+      return 0;
+    }
+    return 1;
+  }
+  my $count = $self->_qc_schema->resultset($class_name)->search_autoqc($query, 1)->count;
+  if ($check_name eq 'insert_size') {
+    my $expected = $self->is_paired_read ? 1 : 0;
+    if ($count != $expected) {
+      carp qq[Expected $expected results got $count for "$desc"];
+      return 0;
+    }
+    return 1;
+  }
+
+  if ($count == 0) {
+    carp qq[Result not found for "$desc"\n];
+    return 0;
+  }
+
+  return 1;
+}
+
+sub _query2string {
+  my ($self, $query) = @_;
+  return sprintf 'check %s, id_run=%i and position=%i and tag index=%s, split %s',
+    $query->{'check'},
+    $self->id_run,
+    $query->{'position'},
+    $query->{'tag_index'} // 'undef',
+    $query->{'subset'} || $query->{'human_split'} || q[none];
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+__END__
+
+=head1 NAME
+
+npg_validation::runfolder::deletable::autoqc
+
+=head1 VERSION
+
+$LastChangedRevision: 18226 $
+
+=head1 SYNOPSIS
+
+  my $rf = npg_validation::runfolder::deletable::autoqc->new(id_run => 1234, verbose => 1);
+  my $is_archived = $rf->fully_archived;
+
+=head1 DESCRIPTION
+
+  Compares a set of archived bam files againsts a set of autoqc results for
+  a run and decides whether all relevant autoqc results have been archived.
+  Autoqc results that can easily be produced again from bam files are omitted.
+  Presence of fastqcheck files in the archive is checked.
+  
+  A full comparison is performed. If at least one autoqc result is missing,
+  the outcome is false, otherwise true is returned. If the verbose attribute
+  is set, a path to each considered bam file is printed to STDERR and a
+  representation of each query to find the autoqc result is printed to STDERR.
+  In non-verbose mode (default) only the queries for missing results are printed.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 irods_files
+
+  A reference to an array of *.bam/*.cram files for a run found in IRODs repository
+
+=head2 is_paired_read
+
+  A flag defining whether there are reverse reads. Currently defaults to true and is not used.
+
+=head2 verbose
+ 
+  A boolean flag switching on and off verbosity.
+
+=head2 exclude_bam
+
+  A boolean option excluding iRODS bam files from consideration.
+
+=head2 skip_checks
+
+  An optional array of autoqc check names to disregard. If a subset is concatenated
+  (use -) with the check name, only this subset will be disregarded for this check.
+
+  Setting this array to [qw/adaptor samtools_stats-phix/] ensure that absence of
+  all adaptor results and absence of samtools_stats results for phix subsets will be
+  disregarded.
+
+=head2 fully_archived
+
+  Returns true if all expected autoqc data are found, otherwise returns false.
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose
+
+=item Readonly
+
+=item Try::Tiny
+
+=item List::MoreUtils
+
+=item npg_qc::Schema
+
+=item npg_qc::autoqc::role::result
+
+=item npg_common::irods::Loader
+
+=item npg_common::irods::iRODSCapable
+
+=item npg_common::irods::Repository
+
+=item npg_tracking::glossary::run
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+Marina Gourtovaia E<lt>mg8@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2016 GRL
+
+This file is part of NPG.
+
+NPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ t/10-validation-sequence_files.t
@@ -0,0 +1,217 @@
+use strict;
+use warnings;
+use Test::More tests => 3;
+use Test::Warn;
+use Test::Exception;
+use File::Temp qw/ tempdir /;
+use Log::Log4perl;
+use File::Slurp qw/ read_file write_file prepend_file /;
+use File::Path qw/ make_path /;
+use List::MoreUtils qw/ none /;
+
+use WTSI::NPG::iRODS;
+
+use_ok('npg_validation::runfolder::deletable::sequence_files');
+
+local $ENV{'http_proxy'} = 'http://wibble.com';
+
+my $dir = tempdir( CLEANUP => 1 );
+my @comp = split '/', $dir;
+my $dname = pop @comp;
+my $IRODS_TEST_AREA1 = "$dname";
+
+my $have_irods_execs = exist_irods_executables();
+my $env_file = $ENV{'WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE'} || q[];
+local $ENV{'IRODS_ENVIRONMENT_FILE'} = $env_file || 'DUMMY_VALUE';
+my $test_area_created = ($env_file && $have_irods_execs) ? create_irods_test_area() : 0;
+
+Log::Log4perl::init_once('./t/log4perl_test.conf');
+my $logger = Log::Log4perl->get_logger(q[]);
+
+my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0, logger => $logger);
+
+sub exist_irods_executables {
+  return 0 unless `which ienv`;
+  return 0 unless `which imkdir`;
+  return 1;
+}
+
+sub create_irods_test_area {
+  diag "creating $IRODS_TEST_AREA1";
+  return !system("imkdir $IRODS_TEST_AREA1");
+}
+
+END {
+  if($test_area_created) {
+    local $ENV{'IRODS_ENVIRONMENT_FILE'} = $env_file;
+    eval {system("irm -r $IRODS_TEST_AREA1")};
+  }
+}
+
+subtest 'file extensions, file names' => sub {
+  plan tests => 11;
+
+  my $v = npg_validation::runfolder::deletable::sequence_files->new(
+     archive_path => q[t/data/runfolder_deletable],
+     collection   => q[/my/c],
+     id_run       => 5174,
+     irods        => $irods);
+  is( $v->file_extension, 'cram', 'default file extension is cram');
+  is( $v->index_file_extension, 'crai', 'index file extension is crai');
+  is( $v->_file_name({position=>1}), '5174_1.cram', 'lane 1 file name');
+  is( $v->_file_name({position=>1, tag_index=>0}), '5174_1#0.cram',
+    'lane 1 tag_index 0 target file name');
+  is( $v->_index_file_name('5174_1#0.cram'), '5174_1#0.cram.crai',
+    'index file name for a cram file');
+
+  $v = npg_validation::runfolder::deletable::sequence_files->new(
+     archive_path   => q[t/data/runfolder_deletable],
+     collection     => q[/my/c],
+     id_run         => 5174,
+     file_extension => 'bam',
+     irods          => $irods);
+  is( $v->file_extension, 'bam', 'default file extension is bam');
+  is( $v->index_file_extension, 'bai', 'index file extension is bai');
+  is( $v->_file_name({position=>1}), '5174_1.bam', 'lane 1 file name');
+  is( $v->_file_name({position=>1, tag_index=>0}), '5174_1#0.bam',
+    'lane 1 tag_index 0 target file name');
+  is( $v->_file_name({position=>1, tag_index=>1}), '5174_1#1.bam',
+    'lane 1 tag_index 1 target file name');
+  is( $v->_index_file_name('5174_1#0.bam'), '5174_1#0.bam.bai',
+    'index file name for a bam file');
+};
+
+subtest 'deletable or not' => sub {
+  my $num_tests = 12;
+  plan tests => $num_tests;
+
+  SKIP: {
+
+    skip 'Test iRODS not available (WTSI_NPG_iRODS_Test_irodsEnvFile not set?)', $num_tests unless $test_area_created;
+
+    my $irods_do = sub {
+      my $command = shift;
+      #diag $command;
+      if (system($command)) {
+        die "Failed to execute command";
+      }
+    };
+  
+    local $ENV{'NPG_CACHED_SAMPLESHEET_FILE'} =
+      't/data/runfolder_deletable/20405/samplesheet_20405.csv';
+
+    my @file_list = read_file('t/data/runfolder_deletable/20405/file_list');
+    @file_list = sort @file_list;
+    my $archive = join q[/], $dir, '20405';
+    my @letters     = (q(a)..q(z));
+    my $num_files   = scalar @file_list;
+    my $empty                 =  5;
+    my $not_aligned           = 15;
+    my $empty_and_not_aligned = 20;
+
+    ok($num_files > $empty_and_not_aligned, 'number of files is sufficiently large');
+ 
+    my $i = 0;
+    my $file_map = {};
+    while ($i < $num_files) {
+      my $file = $file_list[$i];
+      $file =~ s/\s+$//;
+      $file_list[$i] = $file;
+      my ($lane) = $file =~ /^\d+_(\d)/;
+      my $lane_archive = join q[/], $archive, 'lane'.$lane;
+      make_path $lane_archive;
+
+      my $path  = join q[/], $lane_archive, $file;
+      $file_map->{$file} = $path;
+      write_file($path, join(q[,], map {$letters[rand(26)]} (1 .. 30)) ) ;
+      
+      my $ipath = join q[/], $IRODS_TEST_AREA1, $file;
+      $irods_do->("iput -K $path $ipath");
+      my $num_reads = ($i == $empty || $i == $empty_and_not_aligned) ? 0 : int(rand(100));
+      my $align_flag = ($i == $not_aligned || $i == $empty_and_not_aligned) ? 0 : 1;
+
+      if (none {$i == $_} ($empty, $not_aligned, $empty_and_not_aligned)) {
+        $irods_do->("iput $path ${ipath}.crai"); 
+      }
+      $irods_do->("imeta add -d $ipath alignment $align_flag");
+      $irods_do->("imeta add -d $ipath total_reads $num_reads");
+
+      $i++;
+    }
+
+    my $ref = {
+      archive_path   => $archive,
+      collection     => $IRODS_TEST_AREA1,
+      id_run         => 20405,
+      irods          => $irods,
+      is_indexed     => 1,
+    };
+
+    my $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    ok($v->archived_for_deletion(), 'deletable');
+
+    # Remove one of iRODS files
+    my $temp = $file_list[$empty];
+    my $to_remove = join q[/], $IRODS_TEST_AREA1, $temp; 
+    $irods_do->("irm $to_remove");
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    my $result;
+    warning_like { $result = $v->archived_for_deletion() }
+      qr/Number of sequence files is different/,
+      'not deletable - number of files check';
+    ok(!$result, 'not deletable');
+
+    # Replace it with a file with a random name,
+    # so that the number of files is correct.
+    my $wrong = "$IRODS_TEST_AREA1/wrong.cram";
+    my ($f, $path) = each %{$file_map};
+    $irods_do->("iput $path $wrong");
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    warning_like { $result = $v->archived_for_deletion() }
+      qr/According to LIMS, file $temp is missing/,
+      'not deletable - mismatch with lims data';
+    ok(!$result, 'not deletable');
+    $irods_do->("irm $wrong");
+  
+    # Restore previously removed file, excluding metadata
+    $irods_do->(join q[ ], 'iput',  '-K', $file_map->{$temp}, $to_remove);
+    $ref->{'irods'} = WTSI::NPG::iRODS->new(strict_baton_version => 0, logger => $logger);
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    throws_ok { $v->archived_for_deletion() }
+      qr/'alignment' metadata is missing for $to_remove/,
+      'alignment metadata missing - error';
+    $irods_do->("imeta add -d $to_remove alignment 1");
+    $ref->{'irods'} = WTSI::NPG::iRODS->new(strict_baton_version => 0, logger => $logger);
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    throws_ok { $v->archived_for_deletion() }
+      qr/'total_reads' metadata is missing for $to_remove/,
+      'total_reads metadata missing - error';
+    $irods_do->("imeta add -d $to_remove total_reads 0");
+
+    $to_remove = $file_list[$num_files - 1];
+    my $ito_remove = join q[/], $IRODS_TEST_AREA1, $to_remove . '.crai';
+    # Remove an index file
+    $irods_do->("irm $ito_remove");
+    $ref->{'irods'} = WTSI::NPG::iRODS->new(strict_baton_version => 0, logger => $logger);
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    warning_like { $result = $v->archived_for_deletion() }
+      qr/Index file for $to_remove does not exist/,
+      'not deletable - index file is missing';
+    ok(!$result, 'not deletable');
+    # Put it back
+    system("iput -K $path $ito_remove");
+
+    # Preped test to one of the staging files so that
+    # its md5 changes
+    prepend_file($path, 'aaaaaaaa');
+    $ref->{'irods'} = WTSI::NPG::iRODS->new(strict_baton_version => 0, logger => $logger);
+    $v = npg_validation::runfolder::deletable::sequence_files->new($ref);
+    warning_like { $result = $v->archived_for_deletion() }
+      qr/md5 wrong for $f/,
+      'not deletable - md5 mismatch';
+    ok(!$result, 'not deletable');
+  };
+};
+
+1;
+__END__
