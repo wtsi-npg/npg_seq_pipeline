23d65823584e53d3c40dc8f3186c6e4b99465e25
Marina Gourtovaia <mg8@sanger.ac.uk>
2018-01-18 20:37:48 +0000

    set WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE env var for tests running in Travis

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -9,6 +9,8 @@ touch t/bin/software/solexa/bin/blat; chmod +x t/bin/software/solexa/bin/blat
 touch t/bin/software/solexa/bin/bamtofastq; chmod +x t/bin/software/solexa/bin/bamtofastq
 touch t/bin/software/solexa/bin/norm_fit; chmod +x t/bin/software/solexa/bin/norm_fit 
 
+export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
+
 ./Build test --verbose
 
 if [ $? -ne 0 ]; then
