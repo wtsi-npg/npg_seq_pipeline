e27fb2370063abb59a5f5f214a8ae9107760c279
Kevin Lewis <kl2@sanger.ac.uk>
2018-06-01 17:07:29 +0100

    correct splice directives for spike tag processing to work with new p4 templates (NovaSeq)

--- lib/npg_pipeline/function/seq_alignment.pm
+++ lib/npg_pipeline/function/seq_alignment.pm
@@ -329,7 +329,7 @@ sub _alignment_command { ## no critic (Subroutines::ProhibitExcessComplexity)
   }
   else {
     push @{$p4_ops->{prune}}, 'foptgt.*samtools_stats_F0.*00_bait.*-';  # confirm hyphen
-    push @{$p4_ops->{splice}}, 'src_bam:-foptgt_bamsort_coord:', 'foptgt_seqchksum_tee:final-scs_cmp_seqchksum:outputchk';
+    push @{$p4_ops->{splice}}, 'ssfqc_tee_ssfqc:straight_through1:-foptgt_bamsort_coord:', 'foptgt_seqchksum_file:-scs_cmp_seqchksum:outputchk';
   }
 
   my $p4_local_assignments = {};
