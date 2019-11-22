55c8aaeb57d48cbba869dc046ddf284494f93635
mgcam <mg8@sanger.ac.uk>
2019-11-22 13:02:18 +0000

    pin miniconda version

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -6,7 +6,7 @@ set -e -x
 # be skipped
 
 # install conda client
-wget "https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh" -O miniconda.sh;
+wget "https://repo.continuum.io/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh" -O miniconda.sh;
 chmod +x miniconda.sh;
 ./miniconda.sh -b  -p /home/travis/miniconda;
 export PATH=/home/travis/miniconda/bin:$PATH;
