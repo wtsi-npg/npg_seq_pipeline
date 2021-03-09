58393fc1c5310c89777eb8a6df2c27b866a4d05a
Frederick Dodd <fd6@sanger.ac.uk>
2021-03-09 13:23:36 +0000

    Adding github actions files for tests. Assigning env vars in travis_install.sh

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -5,6 +5,14 @@ set -e -x
 # iRODS test server is not set up, so tests that require it will
 # be skipped
 
+#setting environment variables
+WTSI_NPG_GITHUB_URL=$1
+WTSI_NPG_CONDA_REPO=$2
+WTSI_NPG_BUILD_BRANCH=$3
+
+#tmp
+wget -qO - https://cpanmin.us | /usr/bin/perl - --sudo App::cpanminus #installing cpanminus to install modules
+
 # install conda client
 wget "https://repo.continuum.io/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh" -O miniconda.sh;
 chmod +x miniconda.sh;
@@ -17,16 +25,18 @@ conda install --yes --channel ${WTSI_NPG_CONDA_REPO} --channel default --mkdir -
 # install samtools from our conda channel
 # this is needed for our basic IRODS Perl wrapper to work
 conda install --yes --channel ${WTSI_NPG_CONDA_REPO} --channel default --mkdir --prefix /home/travis/miniconda/samtools samtools;
-
+echo "1:1 complete conda install"
 # The default build branch for all repositories. This defaults to
 # TRAVIS_BRANCH unless set in the Travis build environment.
-WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH}
+WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH} #TODO this is missing a value...current github branch to add here instead of travis
 
-# CPAN
+# CPANM install and C compiler
+
 cpanm --quiet --notest Alien::Tidyp # For npg_tracking
 cpanm --quiet --notest Module::Build
 cpanm --quiet --notest Net::SSLeay
 cpanm --quiet --notest https://github.com/chapmanb/vcftools-cpan/archive/v0.953.tar.gz # for npg_qc
+echo "1:2 complete cpanm modules"
 
 # WTSI NPG Perl repo dependencies
 repos=""
@@ -40,6 +50,7 @@ for repo in perl-dnap-utilities perl-irods-wrap ml_warehouse npg_ml_warehouse np
     repos=$repos" /tmp/${repo}.git"
 done
 
+echo "1:3 complete repo branch clones"
 # Install CPAN dependencies. The src libs are on PERL5LIB because of
 # circular dependencies. The blibs are on PERL5LIB because the package
 # version, which cpanm requires, is inserted at build time. They must
@@ -50,20 +61,32 @@ do
     export PERL5LIB=$repo/blib/lib:$PERL5LIB:$repo/lib
 done
 
+echo "1:4 complete perl5lib export"
+
 for repo in $repos
 do
     cd $repo
-    cpanm --quiet --notest --installdeps .
+    echo "2:0: install deps for : $repo"
+    #TODO getting cpanm .
+    #wget -qO - https://cpanmin.us | /usr/bin/perl - --sudo App::cpanminus # try install cpan for each repo
+    
+    #cpanm UUID 2>&1 | tee tmp.log
+
+    cpanm  --quiet --notest --installdeps .
+    
+    #perl -nle 'if (/failed/){ if ( m{(/home\S+/build.log)} ) { print $1; } }' tmp.log | xargs -r cat
     perl Build.PL
     ./Build
 done
+
+echo FINISHED FIRST CPAN INSTALLDEPS LOOP
 
 # Finally, bring any common dependencies up to the latest version and
 # install
 for repo in $repos
 do
     cd $repo
-    cpanm --quiet --notest --installdeps .
+    cpanm  --quiet --notest --installdeps .
     ./Build install
 done
 
